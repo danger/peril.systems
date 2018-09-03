dea1206bade0f4bac8762e838472cd5be2fb38a5
Orta Therox <orta.therox@gmail.com>
2018-09-03 15:25:04 -0400

    Move to gatsby maybe

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/orta/dev/projects/danger/hazmat/peril.systems/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/orta/dev/projects/danger/hazmat/peril.systems/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/orta/dev/projects/danger/hazmat/peril.systems/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/orta/dev/projects/danger/hazmat/peril.systems/src/pages/page-2.js"))
+}
+
